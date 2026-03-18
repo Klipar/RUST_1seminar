0cc666e54a2fff51a5a9a7c53f7f7f3f7a88e1c1
Klipar <bhdnkarpenko123@gmail.com>
2026-03-18 22:18:54 +0100

    add question structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 //! Quizzer library
 
+struct Question {
+    question: String,
+    options: [String; 4],
+}
+
 /// Returns a greeting message
 pub fn get_greeting() -> String {
     String::from("Hello from the quizzer library!")
