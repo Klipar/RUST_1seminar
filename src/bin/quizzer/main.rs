89c37805bdd67516cd7ba8c7a31250a3a4484e0b
Klipar <bhdnkarpenko123@gmail.com>
2026-03-19 23:16:26 +0100

    Arseniy is finished here

--- src/bin/quizzer/main.rs
+++ src/bin/quizzer/main.rs
@@ -14,6 +14,11 @@ struct WorkingMod {
     state: String,
 }
 
+fn read_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("read error");
+    input.trim().to_owned()
+}
 
 fn main() {
     let work_mod = match WorkingMod::try_parse() {
@@ -21,13 +26,13 @@ fn main() {
             if work_mod.state == "question-entering" || work_mod.state == "quiz"{
                 work_mod
             } else {
-                eprintln!("Арсен, напиши норм помилку пліззз, що передано не те що треба");
+                eprintln!("Unknown working mode, please try again");
                 process::exit(1);
             }
         },
         Err(e) => {
             if e.to_string().contains("unexpected argument") {
-                eprintln!("Арсен, напиши норм помилку пліззз");
+                eprintln!("Unexpected argument");
             } else {
                 eprintln!("{}", e);
             }
@@ -37,51 +42,36 @@ fn main() {
 
     if work_mod.state == "question-entering" {
         loop {
-            println!("Enter enter new question ot \"exit\" to exit");
-            let mut input = String::new();
+            println!("Enter new question or \"exit\" to exit the program");
 
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Помилка читання");
+            let question_text = read_input();
 
-            let question_text = input.trim().to_string();
-            input.clear();
             if question_text == "exit"{
                 process::exit(0);
             }
 
-            println!("Enter correct відповідь:");
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Помилка читання");
-
+            println!("Enter correct answer:");
             let mut answers = vec![String::new(); 4];
-            answers[0] = input.trim().to_string(); // add correct answer
-            input.clear();
+            answers[0] = read_input(); // correct answer
 
             for i in 1..4 {
-                println!("Enter incorrect відповідь:");
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Помилка читання");
-
-                answers[i] = input.trim().to_string();
-                input.clear();
+                println!("Enter incorrect answer:");
+                answers[i] = read_input();
             }
 
             let question = Question {
                 question: question_text,
-                options: answers.try_into().expect("Потрібно рівно 4 елементи"),
+                options: answers.try_into().expect("You need exactly 4 answers"),
             };
 
             question.append_to_json();
 
-            println!("Додано новий запис:");
-            // println!("{:?}", question);
+            println!("The new entry has been added:");
         }
     } else {
         let question_set =  import_from_json();
         let mut true_answers = 0;
+
         for q in &question_set{
             println!("\n{}\nOptions:", q.question);
 
@@ -92,16 +82,13 @@ fn main() {
                 println!("{}", q.options[i]);
             }
 
-            let mut user_answer = String::new();
+            let user_answer = read_input();
 
-            io::stdin()
-                .read_line(&mut user_answer)
-                .expect("Помилка читання");
-
-            if user_answer.trim() == q.options[0]{
+            if user_answer == q.options[0]{
                 true_answers+=1;
             }
         }
-        println!("\nyour score: {}/{}", true_answers, question_set.len());
+
+        println!("\nYour score: {}/{}", true_answers, question_set.len());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -25,31 +25,3 @@ pub fn import_from_json() -> Vec<Question> {
         Err(_) => Vec::new(),
     }
 }
-
-pub fn get_question() -> Vec<Question> {
-    let mut questions = Vec::new();
-    questions.push(Question {
-        question: "2 + 2 = ?".to_string(),
-        options: [
-            "3".to_string(),
-            "4".to_string(),
-            "5".to_string(),
-            "6".to_string(),
-        ],
-    });
-
-    questions.push(Question {
-        question: "Столиця Франції?".to_string(),
-        options: [
-            "Лондон".to_string(),
-            "Париж".to_string(),
-            "Берлін".to_string(),
-            "Мадрид".to_string(),
-        ],
-    });
-    questions.push(Question {
-        question: "Otakza od zaka fresko?".to_string(),
-        options: ["Tak".to_string(), "niiii".to_string(), "may be".to_string(), "xto ja".to_string()]
-    });
-    questions
-}
